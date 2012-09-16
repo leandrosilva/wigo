fd263089d8424e06ae7a8090041b1895a946309f
Leandro Silva <leandrodoze@gmail.com>
2012-09-16 19:23:41 -0300

    first commit

---
+++ wigo.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+    Wigo - What is going on?
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    An application and API to keep track on state machines. It's quite useful
+    for monitoring and metrics.
+
+    :copyright: (c) 2012 by Leandro Silva ~ CodeZone.
+    :license: MIT.
+"""
+from flask import Flask, request, session, url_for, redirect, render_template, abort, g, flash
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('WIGO_SETTINGS', silent=True)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+### Here we go!
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
