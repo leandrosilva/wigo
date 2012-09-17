17411f3a63c4644df38006329d2a0b4ad9ed9e3f
Leandro Silva <leandrodoze@gmail.com>
2012-09-16 23:33:55 -0300

    added api, core, and dashboard modules

---
+++ wigo/core.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    Wigo - What is going on?
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    An application and API to keep track on state machines. It's quite useful
+    for monitoring and metrics.
+
+    :copyright: (c) 2012 by Leandro Silva ~ CodeZone.
+    :license: MIT.
+"""
