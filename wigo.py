36734c9f6ba274f680842fb1e01a5a11222dae7b
Leandro Silva <leandrodoze@gmail.com>
2012-09-17 00:36:46 -0300

    added before_filter

--- wigo.py
+++ wigo.py
@@ -20,12 +20,8 @@ app.config.from_object('wigo.config.DefaultSettings')
 app.config.from_envvar('WIGO_SETTINGS', silent=True)
 
 #
-# General
+# Error handling
 #
-
-@app.route('/')
-def home():
-    return redirect(url_for('dashboard_index'))
 
 @app.errorhandler(404)
 def not_found(error):
@@ -34,6 +30,22 @@ def not_found(error):
 @app.errorhandler(500)
 def internal_server_error(error):
     return render_template('500.html'), 500
+
+#
+# Filtering
+#
+
+@app.before_request
+def before_request():
+    pass
+
+#
+# General
+#
+
+@app.route('/')
+def home():
+    return redirect(url_for('dashboard_index'))
 
 #
 # Dashboard
